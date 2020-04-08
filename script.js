07b234869e08ec49e008eb8f5fd5227f7a900825
KarenMkrtchyan <KarenMkrtchyan@users.noreply.github.com>
2020-04-08 14:49:03 +0400

    part 1

--- script.js
+++ script.js
@@ -1,33 +1,30 @@
+const varkyan = document.querySelector(".arrow1");
+const rope = document.querySelector(".arrow2");
+const jam = document.querySelector(".arrow3");
+const clocknumber = document.querySelector(".clocknumber");
 
-var varkyan = document.querySelector(".arrow1");
-var rope = document.querySelector(".arrow2");
-var jam = document.querySelector(".arrow3");
-var clocknumber = document.querySelector(".clocknumber");
+const now = new Date();
 
-var v = 0, r = 0, j = 0;
+let v = now.getSeconds(),
+    r = now.getMinutes(),
+    j = now.getHours();
 
-setInterval(function (){
-    clocknumber.innerHTML = j+":"+r+":"+v;
-    varkyan.style.transform = "rotate("+v*6+"deg)";
+setInterval(function () {
+    clocknumber.innerHTML = j + ":" + r + ":" + v;
+    varkyan.style.transform = "rotate(" + v * 6 + "deg)";
     v++;
-    if(v==60){
+    if (v == 60) {
         r++;
-        v=0;
+        v = 0;
     }
-    rope.style.transform = "rotate("+r*6+"deg)";
-    if(r==60){
+    rope.style.transform = "rotate(" + r * 6 + "deg)";
+    if (r == 60) {
         j++;
-        r=0;
+        r = 0;
     }
-    jam.style.transform = "rotate("+j*30+"deg)";
-    if(j==24){
-        j++; 
-        j=0;
+    jam.style.transform = "rotate(" + j * 30 + "deg)";
+    if (j == 24) {
+        j++;
+        j = 0;
     }
-},1000);
-
-
-
-
-    
-
+}, 1000);
